018f5f17a23cc9918979c840c26a7ce3c06045f0
NoEul <67686234+no-eul@users.noreply.github.com>
2024-07-05 03:13:55 +0900

    Upgrade to mc1.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
-import java.nio.charset.StandardCharsets
-import java.util.Scanner
-
 plugins {
 	id("java")
-	id("fabric-loom") version "1.3-SNAPSHOT"
+	id("fabric-loom") version "latest.release"
 	id("maven-publish")
 }
 
@@ -26,20 +23,15 @@ dependencies {
 	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
 	modImplementation("com.terraformersmc:modmenu:${property("mod_menu_version")}")
 
-	modRuntimeOnly("maven.modrinth:mixintrace:1.1.1+1.17")
-	modRuntimeOnly("maven.modrinth:notenoughcrashes:4.4.5+1.20.1-fabric")
-	modRuntimeOnly("maven.modrinth:language-reload:1.5.8+1.20.1")
-//	modRuntimeOnly("maven.modrinth:smoothboot-fabric:1.19.4-1.7.0")
-	modRuntimeOnly("maven.modrinth:immediatelyfast:1.1.20+1.20.1")
-	modRuntimeOnly("com.github.LlamaLad7:MixinExtras:0.2.0-beta.9")
-	modRuntimeOnly("maven.modrinth:ferrite-core:6.0.0-fabric")
-	modRuntimeOnly("maven.modrinth:lithium:mc1.20.1-0.11.2")
+	modRuntimeOnly("maven.modrinth:mixintrace:latest.release")
 }
 
 loom {
-	sourceSets["main"].resources.files
+	sourceSets.forEach {
+		it.resources.files
 			.find { file -> file.name.endsWith(".accesswidener") }
-			.let(accessWidenerPath::set)
+			?.let(accessWidenerPath::set)
+	}
 
 	@Suppress("UnstableApiUsage")
 	mixin {
@@ -47,36 +39,15 @@ loom {
 	}
 
 	runs {
-		val commonVmArgs = arrayOf(
-				"-Dfabric.systemLibraries=${System.getProperty("java.home")}/lib/hotswap/hotswap-agent.jar",
-				"-XX:+AllowEnhancedClassRedefinition",
-				"-XX:HotswapAgent=fatjar",
-				file(".gradle/loom-cache/remapClasspath.txt")
-						.takeIf { it.exists() && it.isFile && it.canRead() }
-						?.let { Scanner(it, StandardCharsets.UTF_8) }
-						?.use {
-							it.useDelimiter(File.pathSeparator)
-							while (it.hasNext()) {
-								val next = it.next()
-								if ("net.fabricmc${File.separator}sponge-mixin" in next)
-									return@use next
-							}
-							return@use null
-						}
-						?.let { "-javaagent:${it}" }
-		).filterNotNull()
-
 		getByName("client") {
 			configName = "Minecraft Client"
 			runDir = "run/client"
-			vmArgs(commonVmArgs)
 			client()
 		}
 
 		getByName("server") {
 			configName = "Minecraft Server"
 			runDir = "run/server"
-			vmArgs(commonVmArgs)
 			server()
 		}
 	}
@@ -99,10 +70,12 @@ tasks {
 
 	processResources {
 		val inputs = mapOf(
+			"mod_id" to project.property("mod_id"),
 			"version" to project.version,
 			"name" to project.property("mod_name"),
 			"minecraft_version" to project.property("minecraft_version"),
-			"loader_version" to project.property("loader_version")
+			"loader_version" to project.property("loader_version"),
+			"fabric_api_version" to project.property("fabric_api_version"),
 		)
 		this.inputs.properties(inputs)
 		filesMatching("fabric.mod.json") {
@@ -113,12 +86,37 @@ tasks {
 	jar {
 		from("LICENSE.txt")
 		archiveBaseName.set("${project.property("archive_base_name")}")
-		archiveAppendix.set("fabric-mc${project.property("minecraft_version")}")
+		archiveAppendix.set("fabric")
+		archiveClassifier.set("mc${project.property("minecraft_version")}")
 	}
 
 	remapJar {
 		archiveBaseName.set("${project.property("archive_base_name")}")
-		archiveAppendix.set("fabric-mc${project.property("minecraft_version")}")
+		archiveAppendix.set("fabric")
+		archiveClassifier.set("mc${project.property("minecraft_version")}")
+	}
+}
+
+afterEvaluate {
+	loom.runs.configureEach {
+		vmArgs(
+			"-Dfabric.systemLibraries=${System.getProperty("java.home")}/lib/hotswap/hotswap-agent.jar",
+			"-Dfabric.development=true",
+			"-Dfabric.fabric.debug.deobfuscateWithClasspath",
+			"-Dmixin.debug.export=true",
+			"-Dmixin.debug.verify=true",
+//			"-Dmixin.debug.strict=true",
+			"-Dmixin.debug.countInjections=true",
+			"-Dmixin.hotSwap=true",
+			"-XX:+AllowEnhancedClassRedefinition",
+			"-XX:HotswapAgent=fatjar",
+			"-XX:+IgnoreUnrecognizedVMOptions",
+			"-javaagent:${
+				configurations.compileClasspath.get()
+					.files { it.group == "net.fabricmc" && it.name == "sponge-mixin" }
+					.first()
+			}"
+		)
 	}
 }
 
--- src/main/java/dev/noeul/fabricmod/template/FabricModTemplate.java
+++ src/main/java/dev/noeul/fabricmod/template/FabricModTemplate.java
@@ -1,4 +1,4 @@
-package dev.noeul.fabricmod.modid;
+package dev.noeul.fabricmod.template;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
@@ -6,8 +6,10 @@ import net.fabricmc.loader.api.ModContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ModName implements ModInitializer {
-	public static final ModContainer MOD = FabricLoader.getInstance().getModContainer("modid").orElseThrow(NullPointerException::new);
+public class FabricModTemplate implements ModInitializer {
+	public static final ModContainer MOD = FabricLoader.getInstance()
+			.getModContainer("template")
+			.orElseThrow(NullPointerException::new);
 	public static final String ID = MOD.getMetadata().getId();
 	public static final String NAME = MOD.getMetadata().getName();
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAME);
